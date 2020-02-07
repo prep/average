e84d5e710d5104b2ddf037d16847eccb5ad1eaf2
Ingo Oppermann <ingo@oppermann.ch>
2020-02-07 11:34:40 +0100

    Fix leaking ticker
    The ticker needs to be stopped if it is not used anymore, otherwise the associated resources will not be released (https://golang.org/pkg/time/#NewTicker)

--- slidingwindow.go
+++ slidingwindow.go
@@ -56,6 +56,7 @@ func New(window, granularity time.Duration) (*SlidingWindow, error) {
 
 func (sw *SlidingWindow) shifter() {
 	ticker := time.NewTicker(sw.granularity)
+	defer ticker.Stop()
 
 	for {
 		select {
